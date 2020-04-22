d527927b86b3b10e1355ce89de96b5e196e9c249
higashiM <eastwg@gmail.com>
2020-04-22 16:00:06 +0100

    testing server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
+const { schema } = require("../db/graphql/schema/index");
+const cors = require("cors");
+app.use(cors());
+
 /* app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 }); */
