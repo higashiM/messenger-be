2e48c9d124800526f4ee30b8fca2e4c673acc165
higashiM <eastwg@gmail.com>
2020-04-22 15:41:15 +0100

    testing server

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var port = process.env.PORT || 3000;
 
-app.get("/", function (req, res) {
+/* app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
-});
+}); */
 
 io.on("connection", function (socket) {
+  socket.on("join", function () {
+    io.emit("welcome");
+  });
   socket.on("chat message", function (msg) {
     io.emit("chat message", msg);
   });
