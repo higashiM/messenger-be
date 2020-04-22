381199c5ac93a510b9ff13949e7890a8617ee8ad
higashiM <eastwg@gmail.com>
2020-04-22 15:23:39 +0100

    testing server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.get("/", function (req, res) {
 });
 
 io.on("connection", function (socket) {
-  console.log("new client connected");
+  /*   console.log("new client connected"); */
   socket.on("chat message", function (msg) {
     io.emit("chat message", msg);
   });
